aa9dcf17eaaea255219b0b799abac6852e196648
kenenisa <kenenisaalemayhu0@gmail.com>
2023-03-12 00:36:18 +0300

    data update

--- node.js
+++ node.js
@@ -36,7 +36,7 @@ function scrape(file, contestI) {
 }
 
 let result = [];
-for (let i = 1; i <= 11; i++) {
+for (let i = 1; i <= 12; i++) {
     let con = scrape(`txt/contest#${i}.txt`, i)
     con.contest = i
     result.push(con)
