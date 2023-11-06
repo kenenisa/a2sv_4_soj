eb2c7d972173f96c40b8477fc75c53d620f50d9d
Kenenisa Alemayehu <53818162+kenenisa@users.noreply.github.com>
2023-11-06 12:43:08 +0300

    updated data

--- node.js
+++ node.js
@@ -8,7 +8,6 @@ function scrape(file, contestI) {
     const doc = dom.window.document;
     const problems = doc.querySelector(".standings tr").children.length - 4
     const table = doc.querySelector(".standings tbody").children
-    console.log(table.length);
     const people = []
     for (let i = 1; i < table.length; i++) {
         let n = table[i].children[1].children
@@ -19,15 +18,17 @@ function scrape(file, contestI) {
             }
         }
         const rank = Number(table[i].children[0].textContent);
+        const penalty = Number(table[i].children[3].textContent);
         const solved = Number(table[i].children[2].textContent);
         if (rank > 0) {
             // if (contestI > 1) {
-                names.add(n.textContent)
+            names.add(n.textContent)
             // }
             const person = {
                 rank,
                 name: n.textContent,
-                solved
+                solved,
+                penalty
             }
             people.push(person)
         }
@@ -36,10 +37,15 @@ function scrape(file, contestI) {
 }
 
 let result = [];
-for (let i = 1; i <= 17; i++) {
-    let con = scrape(`txt/contest#${i}.txt`, i)
-    con.contest = i
-    result.push(con)
+for (let i = 1; i <= 43; i++) {
+    try {
+
+        let con = scrape(`txt/contest#${i}.txt`, i)
+        con.contest = i
+        result.push(con)
+    } catch (e) {
+        console.log(e)
+    }
 }
 function findKid(name) {
     const r = []
@@ -47,7 +53,7 @@ function findKid(name) {
     result.forEach(contest => {
         const found = contest.people.find(p => p.name == name)
         r.push(found ? found.rank : -1)
-        s.push(found ? (found.solved * 100) / contest.problems: '0')
+        s.push(found ? (found.solved * 100) / contest.problems : '0')
     })
     return { r, n: name, s }
 }
@@ -55,20 +61,20 @@ const kids = []
 names.forEach(name => {
     kids.push(findKid(name))
 })
-var g47 = ['epix','p4ndish','super3codes','nabroleonx','Simon_Ghiwot','ebdollasign','J0RdN','mafilala','yonaries','kenenisa','abudah29','Bura_B','ahmedin','se348']
-result = result.map(e=>{
-    e.people = e.people.filter(val=>g47.find(b=>b==val.name))
+var g47 = ['epix', 'p4ndish', 'super3codes', 'nabroleonx', 'Simon_Ghiwot', 'ebdollasign', 'J0RdN', 'mafilala', 'yonaries', 'kenenisa', 'abudah29', 'Bura_B', 'ahmedin', 'se348']
+result = result.map(e => {
+    e.people = e.people.filter(val => g47.find(b => b == val.name))
     return e
-}).map(e=>{
-    e.people = e.people.sort((a,b)=>a.rank-b.rank).map((val,i)=>{
-        val.rank = i+1
+}).map(e => {
+    e.people = e.people.sort((a, b) => a.rank - b.rank).map((val, i) => {
+        val.rank = i + 1
         return val
     })
     return e
 })
 const g47Members = []
-g47.forEach(name=>{
+g47.forEach(name => {
     g47Members.push(findKid(name))
 })
-fs.writeFileSync("result.json", JSON.stringify({ raw: result, kids,g47Members }))
+fs.writeFileSync("result.json", JSON.stringify({ raw: result, kids, g47Members }))
 console.log(result);
